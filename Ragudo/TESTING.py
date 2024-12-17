b12aeb9074ef4808c3d446466484288c1a6abb60
gb8462 <planetes8462@gmail.com>
2024-12-17 20:51:44 +0800

    notes

--- Ragudo/TESTING.py
+++
@@ -1,3 +0,0 @@
-print("This Is just a test for merging nothing important get out of here!")
-print("Well Its not working I wonder why")
-print("Update: Somehow managed to figure it out, Got the acheivement")
