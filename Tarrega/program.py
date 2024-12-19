4e4e5fce55244557764003c52afb369d7e145762
Ren <tterencemartin@gmail.com>
2024-12-20 00:51:41 +0800

    Update program.py

--- Tarrega/program.py
+++ Tarrega/program.py
@@ -1,6 +1,1 @@
-#Tarrega Files
-
-def act():
-    print("I am the best")
-    print("Progress made.... 0")
-
+print(""" To be or not to be, that is not really the question I needed to ask rn but yeah, love is war """)
