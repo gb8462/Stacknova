6c5c76baca3d55d5a821545a2c13062bf92d5f56
gb8462 <planetes8462@gmail.com>
2025-03-20 19:28:04 +0800

    update

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-function redirectToPage(url) {
-  window.location.href = url;
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('DOM fully loaded');
-  document.querySelector('.Home').addEventListener('click', () => {
-      console.log('Home clicked');
-      redirectToPage('index.html');
-  });
-
-  document.querySelector('.Credits').addEventListener('click', () => {
-      console.log('Credits clicked');
-      redirectToPage('Credits.html');
-  });
-});
+// nothing here yet
